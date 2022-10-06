90dd74a547802b64ac49a496a050d8626152c66a
Kinshu Jain <kinshujain0@gmail.com>
2022-10-06 22:36:46 +0530

    added digital clock

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ setInterval(() => {
     mtime = d.getMinutes();
     stime = d.getSeconds();
 
-    h = 30 * htime + mtime/2;
+    h = 30 * htime + mtime / 2;
     m = 6 * mtime;
     s = 6 * stime;
 
@@ -12,5 +12,19 @@ setInterval(() => {
     min.style.transform = `rotate(${m}deg)`;
     seconds.style.transform = `rotate(${s}deg)`;
 
+    am = "AM";
+    if (htime > 12) {
+        htime -= 12;
+        am = "PM";
+    }
+    if (htime == 0) {
+        htime = 12;
+    }
+    htime = (htime < 10) ? "0" + htime : htime;
+    mtime = (mtime < 10) ? "0" + mtime : mtime;
+    stime = (stime < 10) ? "0" + stime : stime;
+
+    time = htime + ":" + mtime + ":" + stime + " "+am;
+    document.getElementById("digital").innerText = time;
 
 }, 1000)
